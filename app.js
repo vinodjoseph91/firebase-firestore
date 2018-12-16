4cce53f7d6c9eeeeb983fcdcd0a385a241b7ddbc
vinod.joseph <vinod.joseph@target.com>
2018-12-16 11:41:21 +0530

    added method to delete an item from firebase db

--- app.js
+++ app.js
@@ -7,14 +7,26 @@ function renderCafe(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent ='x';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
     cafeList.appendChild(li);
+
+    // Adding a click event to the cross added above
+    cross.addEventListener('click',(e)=>{
+        e.stopPropagation();
+        // getting the id of the selected item
+        let id = e.target.parentElement.getAttribute('data-id');
+        // deleting the value from the firebase db
+        db.collection('cafes').doc(id).delete();
+    })
 
 }
 
@@ -29,8 +41,11 @@ db.collection('cafes').get().then((snapshot) => {
 //saving data to firebase db
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    db.collections('cafes').add({
-        name : form.name.value,
-        city : form.city.value
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
     });
+
+    form.name.value = '';
+    form.city.value = '';
 })
